ba45b705d928e580e05980066c16b7ccde242f65
Andrei Sorokin <72672144+AndreiSorokin@users.noreply.github.com>
2024-05-03 01:42:16 +0300

    design added to experience

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -11,54 +11,45 @@ import helsinki from "../img/helsinki.jpg";
 
 const Experience = () => {
 
-   useEffect(() => {
-      gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-      
-      if (ScrollTrigger.isTouch !== 1) {
-        ScrollSmoother.create({
-          wrapper: '.wrapper',
-          content: '.experience__content',
-          smooth: 1.5,
-          effects: true
-        });
-  
-        gsap.fromTo('.hero-section', { opacity: 1 }, {
-          opacity: 0,
-          scrollTrigger: {
-            trigger: '.hero-section',
-            start: 'center',
-            end: '820',
-            scrub: true
-          }
-        });
-  
-        let itemsL = gsap.utils.toArray('.gallery__left .gallery__item');
-        itemsL.forEach(item => {
-          gsap.fromTo(item, { opacity: 0, x: -50 }, {
-            opacity: 1, x: 0,
-            scrollTrigger: {
-              trigger: item,
-              start: '-850',
-              end: '-100',
-              scrub: true
-            }
-          });
-        });
-  
-        let itemsR = gsap.utils.toArray('.gallery__right .gallery__item');
-        itemsR.forEach(item => {
-          gsap.fromTo(item, { opacity: 0, x: 50 }, {
-            opacity: 1, x: 0,
-            scrollTrigger: {
-              trigger: item,
-              start: '-750',
-              end: 'top',
-              scrub: true
-            }
-          });
-        });
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.fromTo('.hero-section', { opacity: 1 }, {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: '.hero-section',
+        start: 'center',
+        end: '820',
+        scrub: true
       }
-    }, []);
+    })
+    
+    gsap.utils.toArray('.gallery__left .gallery__item').forEach(item => {
+      gsap.fromTo(item, { x: '-50%', opacity: 0 }, {
+        x: '0%', opacity: 1, scrollTrigger: {
+          trigger: item,
+          start: '50',
+          end: 'top',
+          scrub: true
+        }
+      });
+    });
+  
+    gsap.utils.toArray('.gallery__right .gallery__item').forEach(item => {
+      gsap.fromTo(item, { opacity: 0, x: '50%' }, {
+        opacity: 1, x: 0, scrollTrigger: {
+          trigger: item,
+          start: '50',
+          end: 'top',
+          scrub: true
+        }
+      });
+    });
+  
+    return () => {
+      ScrollTrigger.getAll().forEach(instance => instance.kill());
+    };
+  }, []);
 
   return (
     <div className='experience'>
@@ -77,27 +68,59 @@ const Experience = () => {
 			<div className="portfolio">
 				<div className="portfolio__container">
 					<main className="portfolio__gallery">
-	
+
+          <div data-speed=".9" className="mobile gallery__right">
+            <h2 className="text-block__h">Sovkombank</h2>
+						<p className="text-block__p">A bank I had a role of front-end developer intern.<br/> During this internship I elevated project design and implementation, as well as enhanced end-to-end testing
+              efficacy, resulting in increase in user satisfaction</p>
+          </div>
+
 						<div data-speed=".9" className="gallery__left">
-		
 							<img className="gallery__item" src={bank} alt="Alt"/>		
 							<div className="text-block gallery__item">
-								<h2 className="text-block__h">Creative floating scroll with amazing parallax effect</h2>
-								<p className="text-block__p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor amount scrolling.</p>
+              <div className="pc">
+                <h2 className="text-block__h">Dobrocenter</h2>
+							  	<p className="text-block__p">123</p>
+              </div>
 							</div>
-				
+						</div>
+            
+
+            <div data-speed=".9" className="mobile gallery__left">
+            <h2 className="text-block__h">dobrocenter</h2>
+						<p className="text-block__p">123</p>
+          </div>
+
+						<div data-speed=".9" className="gallery__right">
+							<div className="text-block gallery__item">
+              <div className='pc'>
+                <h2 className="text-block__h">Sovkombank</h2>
+							    <p className="text-block__p">A bank I had a role of front-end developer intern.<br/> During this internship I elevated project design and implementation, as well as enhanced end-to-end testing
+                  efficacy, resulting in increase in user satisfaction</p>
+              </div>
+							</div>
+							<img className="gallery__item" src={dobrocenter} alt="Alt"/>
 						</div>
 
-						<div data-speed="1.1" className="gallery__right">
-		
+            <div data-speed=".9" className="gallery__right">
 							<div className="text-block gallery__item">
-								<h2 className="text-block__h">Creative floating scroll with amazing parallax effect</h2>
-								<p className="text-block__p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor amount scrolling.</p>
+              <div className='mobile '>
+                <h2 className="text-block__h">new code block</h2>
+							    <p className="text-block__p">123</p>
+              </div>
+              <img className="gallery__item" src={dobrocenter} alt="Alt"/>
 							</div>
-		
-							<img className="gallery__item" src={dobrocenter} alt="Alt"/>
-		
 						</div>
+
+            <div data-speed=".9" className="pc gallery__left">
+              <div className="text-block gallery__item">
+                <div className='pc'>
+                  <h2 className="text-block__h">new code block</h2>
+                  <p className="text-block__p">123</p>
+                </div>
+              </div>
+              <img className="gallery__item" src={dobrocenter} alt="Alt"/>
+            </div>
 
 					</main>
 				</div>
